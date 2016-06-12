0e9eec8269a9122f5cd83096c02f3268d5a2bb0f
Gayan Weerakutti <gayan@linuxdeveloper.space>
2016-06-12 13:10:21 +0530

    Advanced HTML Parsing

---
+++ AdvancedHTMLParsing.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+def getHTML(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    return html
+    
+    
+html = getHTML("http://www.pythonscraping.com/pages/warandpeace.html")
+bsObj = BeautifulSoup(html)
+
+nameList = bsObj.findAll("span", {"class":"green"})
+for name in nameList:
+    print(name.get_text())
+
+nameList = bsObj.findAll(text="the prince")
+print(len(nameList))
